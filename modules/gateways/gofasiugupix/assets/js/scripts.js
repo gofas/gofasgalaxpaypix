f0266a4b8ced2617eb09d1827d1ff2f211cdc264
mauriciogofas <mauricio@gofas.net>
2023-03-10 21:17:19 -0300

    start

--- modules/gateways/gofasiugupix/assets/js/scripts.js
+++ modules/gateways/gofasiugupix/assets/js/scripts.js
@@ -1,7 +1,7 @@
 /**
- * Módulo GalaxPay Pix para WHMCS
+ * Módulo iugu Pix para WHMCS
  * @copyright	2022 Gofas Software
- * @see			https://gofas.net/?p=14685
+ * @see			https://gofas.net/?p=14950
  * @license		https://gofas.net/?p=9340
  * @support		https://gofas.net/?p=14690
  * @version		0.1.0
@@ -9,7 +9,7 @@
 $(document).ready(function () {
     var system_url = $("#system_url").val();
     var invoice_id = $("#invoice_id").val();
-    var get_url = "modules/gateways/gofasgalaxpaypix/includes/callback.php";
+    var get_url = "modules/gateways/gofasiugupix/includes/callback.php";
     setInterval(function () {
         $.get(
             system_url + get_url,
